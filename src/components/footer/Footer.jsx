9d8672f8ce64502d36649f34236c31ea6a882e11
Hanan-Nathem-Saadeh <hanan.nathem111@gmail.com>
2022-03-02 08:55:11 +0300

    class-16

---
+++ src/components/footer/Footer.jsx
@@ -0,0 +1,16 @@
+import "./Footer.css";
+function Footer()
+{
+return
+{
+    <div>
+        <nav>
+ <a href="http://google.com">Google</a>
+ <a href="http://facebook.com">Facebook</a>
+ <a href="https://instagram.com">Instagram</a>
+ </nav>
+ <p> &copy; All rights reserved 2022</p>
+    </div>
+}
+}
+export default Footer;
